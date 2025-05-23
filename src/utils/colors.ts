46a877d77cc3e6a8c9b9aa005f120a46d5f029cc
bnasar1 <bakhtawarnasar8@gmail.com>
2025-05-23 13:03:05 -0700

    conflict

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const colors = {
  midNightBlue: '#1b2634',
  white: '#fff',
@@ -7,14 +6,4 @@ export const colors = {
  WhatsappGreen: '#1ebe57',
  OxfordBlue: '#0d1b2a',
  desertSand: '#d9ab72',
-=======
-export const colors = {
- midNightBlue: '#1b2634',
- white: '#fff',
- charcoalBlue: '#1c2237',
- GOLD: '#c9a57e',
- WhatsappGreen: '#1ebe57',
- OxfordBlue: '#0d1b2a',
- desertSand: '#d9ab72',
->>>>>>> 194b4436d8ac3dfa1e5d02a46142ce498007e666
 }
